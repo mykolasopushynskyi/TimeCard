ae8e7dc3ea18514ebd57174381c79804579af0be
mykola <mykola@mykola-wds>
2013-11-08 17:52:17 +0200

    Added check for nullPointerException which makes App unstable.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,34 +20,35 @@ public class Application extends Controller {
 	public static void getTeamList() {
 		Gson gson = new Gson();
 
-        List<Team> teams = Team.find("order by name asc").fetch();
-        List<String> data = new LinkedList<String>();
+		List<Team> teams = Team.find("order by name asc").fetch();
+		List<String> data = new LinkedList<String>();
 
-		for(Team t: teams) {
-              data.add(t.name);
-        }
+		for (Team t : teams) {
+			data.add(t.name);
+		}
 
 		renderJSON(gson.toJson(data));
 	}
 
-    public static void getUserStories() {
-        Gson gson = new Gson();
+	public static void getUserStories() {
+		Gson gson = new Gson();
 
-        List<UserStory> teams = UserStory.find("order by userStoryID asc").fetch();
+		List<UserStory> teams = UserStory.find("order by userStoryID asc")
+				.fetch();
 
-        JsonArray data = new JsonArray();
+		JsonArray data = new JsonArray();
 
-        for(UserStory t: teams) {
-            JsonObject team = new JsonObject();
+		for (UserStory t : teams) {
+			JsonObject team = new JsonObject();
 
-            team.addProperty("id", t.userStoryID);
-            team.addProperty("desc", t.userStoryDesc);
+			team.addProperty("id", t.userStoryID);
+			team.addProperty("desc", t.userStoryDesc);
 
-            data.add(team);
-        }
+			data.add(team);
+		}
 
-        renderJSON(gson.toJson(data));
-    }
+		renderJSON(gson.toJson(data));
+	}
 
 	public static void index() {
 
@@ -55,17 +56,20 @@ public class Application extends Controller {
 		JsonObject me = null;
 		String email = null;
 		boolean isLogged = false;
-
-		if (u != null && u.access_token != null) {
-			me = WS.url(
-					"https://www.googleapis.com/oauth2/v1/userinfo?access_token=%s",
-					WS.encode(u.access_token)).get().getJson()
-					.getAsJsonObject();
-			System.out.println(me);
-			email = me.get("email").getAsString();
-			isLogged = true;
+		try {
+			if (u != null && u.access_token != null) {
+				me = WS.url(
+						"https://www.googleapis.com/oauth2/v1/userinfo?access_token=%s",
+						WS.encode(u.access_token)).get().getJson()
+						.getAsJsonObject();
+				//System.out.println(me);
+				email = me.get("email").getAsString();
+				isLogged = true;
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
-
+		
 		render(email, isLogged);
 	}
 }
