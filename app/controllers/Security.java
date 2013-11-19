9c51b8e4d77139ce5174b401c3c4d882382235a8
mykola <mykola@mykola-wds>
2013-11-19 18:19:43 +0200

    Security controller now using GoogleSocial for authentication with Google account

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -5,93 +5,51 @@ import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 
-import models.User;
 import play.libs.WS;
-import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.Scope;
 import play.mvc.results.Redirect;
+import social.GoogleSocial;
 
-import com.google.gdata.util.common.base.StringUtil;
 import com.google.gson.JsonObject;
 
-public class Security extends Controller/* extends Secure.Security */{
+public class Security extends Controller{
 
-	public static final String authURL = "https://accounts.google.com/o/oauth2/auth";
-	public static final String tokenURL = "https://accounts.google.com/o/oauth2/token";
-	public static final String clientId = "752848984220.apps.googleusercontent.com";
-	public static final String clientSecret = "xrbD2KH0MWHKxgj2mn2ktlUJ";
-
+	
+	private static GoogleSocial socialGoogle = new GoogleSocial(session, redirectURL());
+	
 	public static void logout() {
-		if (getToken() != null) {
-			WS.url("https://accounts.google.com/o/oauth2/revoke?token=%s",
-					getToken()).post();
-		}
-		session.clear();
+		socialGoogle.logout();
 		Application.index();
 	}
 
 	public static void authGoogle() {
-		if (Scope.Params.current().get("code") != null) {
-			String accessCode = Scope.Params.current().get("code");
-			Map<String, Object> params = new HashMap<String, Object>();
-			params.put("client_id", clientId);
-			params.put("client_secret", clientSecret);
-			params.put("redirect_uri", redirectURL());
-			params.put("code", accessCode);
-			params.put("grant_type", "authorization_code");
-			WS.HttpResponse response = WS.url(tokenURL).params(params).post();
-
-			String accessToken = null;
-
-			JsonObject respObj = (JsonObject) response.getJson();
-
-			if (respObj.has("access_token")) {
-				accessToken = respObj.get("access_token").getAsString();
-				session.put("token", accessToken);
-			}
-
-			Application.index();
-		}
+		socialGoogle.loginToSocial();
 	}
 
 	public static void tryAuthGoogle() {
-		throw new Redirect(authURL + "?client_id=" + clientId
-				+ "&redirect_uri=" + redirectURL() + "&response_type=code"
-				+ "&scope=https://www.googleapis.com/auth/userinfo.email");
+		socialGoogle.redirectToSocial();
 	}
 
 	static String redirectURL() {
 		return play.mvc.Router.getFullUrl("Security.authGoogle");
 	}
 
+
+	static boolean isLogged() {
+		return socialGoogle.isLogged();
+	}
+
 	static String getToken() {
 		String result = session.get("token");
-
+		
 		if (StringUtils.isBlank(result)) {
 			return null;
 		} else {
 			return result;
 		}
 	}
-
-	static boolean isLogged() {
-		String token = getToken();
-		JsonObject info = null;
-		boolean isLogged = false;
-
-		try {
-			if (token != null) {
-				if (getUserInfo().get("email").getAsString() != null) {
-					isLogged = true;
-				}
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return isLogged;
-	}
-
+	
 	static JsonObject getUserInfo() {
 		String token = getToken();
 		JsonObject userInfo = null;
@@ -108,5 +66,4 @@ public class Security extends Controller/* extends Secure.Security */{
 
 		return userInfo;
 	}
-
 }
