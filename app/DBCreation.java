80cfc6e06e228511f1680c57e42fd51c3bdea929
WDS-GLP <WDS-GLP@globallogic.com>
2013-11-27 14:04:29 +0200

    Fixed job

--- app/DBCreation.java
+++ app/DBCreation.java
@@ -6,12 +6,12 @@ import play.test.Fixtures;
  * Created with IntelliJ IDEA. User: dendy4 Date: 11/4/13 Time: 3:17 PM To
  * change this template use File | Settings | File Templates.
  */
-//@OnApplicationStart
+@OnApplicationStart
 public class DBCreation extends Job {
 
 	public void doJob() {
 
-		Fixtures.deleteAllModels();
-		Fixtures.loadModels("rallydata.yml");
+//		Fixtures.deleteAllModels();
+//		Fixtures.loadModels("rallydata.yml");
 	}
 }
