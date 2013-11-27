aedd1e76dd5d7c4cea2ab5fba5acfcc507b04905
WDS-GLP <WDS-GLP@globallogic.com>
2013-11-27 16:27:24 +0200

    Added some methods

--- app/models/TeamTask.java
+++ app/models/TeamTask.java
@@ -18,7 +18,7 @@ public class TeamTask extends GenericModel {
 	public TeamTask() {
 		this.updatedDate = new Date();
 	}
-	
+
 	public TeamTask(Team team, Task task) {
 		this();
 		this.id.team = team;
@@ -37,8 +37,38 @@ public class TeamTask extends GenericModel {
 		@ManyToOne
 		@JoinColumn(name = "taskId")
 		public Task task;
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((task == null) ? 0 : task.hashCode());
+			result = prime * result + ((team == null) ? 0 : team.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			PK other = (PK) obj;
+			if (task == null) {
+				if (other.task != null)
+					return false;
+			} else if (!task.equals(other.task))
+				return false;
+			if (team == null) {
+				if (other.team != null)
+					return false;
+			} else if (!team.equals(other.team))
+				return false;
+			return true;
+		}
 	}
-	
 
 	public Date getUpdatedDate() {
 		return updatedDate;
@@ -49,4 +79,5 @@ public class TeamTask extends GenericModel {
 	}
 
 	public Date updatedDate;
+
 }
