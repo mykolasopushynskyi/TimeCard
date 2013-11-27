5303be8328fd66e934830eb06e5852daa3196b36
WDS-GLP <WDS-GLP@globallogic.com>
2013-11-27 14:20:01 +0200

    Added script for updating project

---
+++ updateProject.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+PROJECT_DIR='TimeCard';
+
+if [ -d "$PROJECT_DIR"]; then
+  echo 'Stopping app...'
+  cd TimeCard
+  play stop
+  echo 'Deleting old project...'
+  cd ../
+  rm -rf TimeCard
+fi
+
+if [ -f "master.zip"]; then
+  rm master.zip
+fi
+
+echo 'Downloading sources from repo...'
+wget https://github.com/mykolasopushynskyi/TimeCard/archive/master.zip
+
+echo 'Extracting archive...'
+unzip -q master.zip
+mv TimeCard-master/ TimeCard
+
+cd TimeCard/
+play deps --sync
+play precompile
+
+nohup play start -Dprecompiled=true &
+
